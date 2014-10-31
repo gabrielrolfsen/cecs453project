3cfdce445addf8288b1ef58b685eb4d0f0f763ab
Gabriel R. <gabriel.rolfsen@gmail.com>
2014-10-30 18:13:25 -0700

    Created Splash Screen. Icons and images are just temporary. Please tell me what you guys think about it.

--- src/edu/csulb/android/cecs453project/MainActivity.java
+++ src/edu/csulb/android/cecs453project/MainActivity.java
@@ -1,40 +1,7 @@
 package edu.csulb.android.cecs453project;
 
-import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
 
+public class MainActivity extends Activity{
 
-public class MainActivity extends ActionBarActivity {
-
-    @SuppressLint("NewApi") @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
