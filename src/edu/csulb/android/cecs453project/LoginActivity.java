bddfa522cd910e1caf33595d1ed2212b98bd0cd8
Gabriel R. <gabriel.rolfsen@gmail.com>
2014-11-05 16:06:18 -0800

    Updated all Activities. Still working on it.

--- src/edu/csulb/android/cecs453project/LoginActivity.java
+++ src/edu/csulb/android/cecs453project/LoginActivity.java
@@ -1,18 +1,68 @@
 package edu.csulb.android.cecs453project;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity {
+	
+	protected static final String SESSION_KEY = "SESSION_STATUS";
+	Button btnLogin;
+	EditText edtLogin;
+	EditText edtPass;
+	Editor prefEditor;
+	
 
-    @SuppressLint("NewApi") @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+		SharedPreferences pref = getApplicationContext().getSharedPreferences("appPref", 0);
+		prefEditor = pref.edit();
+        
         setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edtLogin = (EditText) findViewById(R.id.edtLogin);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+        
+        
+        /** Checks the login information and 
+         * proceed to the main screen **/
+        btnLogin.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String login = edtLogin.getText().toString();
+				String pass = edtPass.getText().toString();
+				if (attemptLogin(login, pass)){
+					prefEditor.putBoolean(SESSION_KEY, true);
+					prefEditor.commit();
+					Intent inLogin = new Intent(getBaseContext(),MainActivity.class);
+					LoginActivity.this.startActivity(inLogin);
+				}else{
+					Toast.makeText(getBaseContext(), "Login failed.", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+    }
+    
+    private boolean attemptLogin(String login, String pass){
+    	
+    	/** TODO: Request database access and check credentials **/
+    	if(login.equals("admin") && pass.equals("admin"))
+    		return true;
+    	
+    	return false;
     }
 
 
--- src/edu/csulb/android/cecs453project/MainActivity.java
+++ src/edu/csulb/android/cecs453project/MainActivity.java
@@ -1,7 +1,48 @@
 package edu.csulb.android.cecs453project;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity{
+public class MainActivity extends ActionBarActivity {
+	
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/* Checks if the user is logged in */
+		SharedPreferences pref = getApplicationContext().getSharedPreferences("appPref", 0);
+		Log.d("DEBUG",String.valueOf(pref.getBoolean(LoginActivity.SESSION_KEY, false)));
+		if (!pref.getBoolean(LoginActivity.SESSION_KEY, false)){
+			Intent inLoginRequired = new Intent(getBaseContext(), LoginActivity.class);
+			startActivity(inLoginRequired);
+			finish();
+		}
+		setContentView(R.layout.activity_main);
+		
+		Intent inLoginSuccess = getIntent();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
